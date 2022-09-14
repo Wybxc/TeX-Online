f2359ee707a1d01511e5aedef9b68068d4d5f8d1
忘忧北萱草 <wybxc@qq.com>
2022-09-14 06:02:18 +0000

    template

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,4 +6,5 @@ wget https://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
 zcat install-tl-unx.tar.gz | tar xf -
 cd install-tl-*
 sudo perl ./install-tl --no-interaction --scheme=small
-export PATH=$PATH:/usr/local/texlive/2022/bin/x86_64-linux
+sudo /usr/local/texlive/2022/bin/x86_64-linux/tlmgr install ieeetrans ctex
+echo "export PATH=$PATH:/usr/local/texlive/2022/bin/x86_64-linux" >>~/.bashrc
