b24f41c4666a28f7c0d500ee9450581d4063f9df
忘忧北萱草 <wybxc@qq.com>
2022-09-15 06:01:02 +0000

    diagbox

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,7 @@ zcat install-tl-unx.tar.gz | tar xf -
 cd install-tl-*
 sudo perl ./install-tl --no-interaction --scheme=small
 cd ..
-sudo /usr/local/texlive/2022/bin/x86_64-linux/tlmgr install ieeetrans ctex latexindent
+sudo /usr/local/texlive/2022/bin/x86_64-linux/tlmgr install ieeetrans ctex latexindent diagbox pict2e
 wget http://cpanmin.us/ -O cpanm
 chmod +x cpanm
 sudo ./cpanm File::HomeDir YAML::Tiny
