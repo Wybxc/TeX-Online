cc6f724ec38cae602c62771018e154eb5685641b
忘忧北萱草 <wybxc@qq.com>
2022-09-15 05:26:32 +0000

    latexindent

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,5 +6,9 @@ wget https://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
 zcat install-tl-unx.tar.gz | tar xf -
 cd install-tl-*
 sudo perl ./install-tl --no-interaction --scheme=small
-sudo /usr/local/texlive/2022/bin/x86_64-linux/tlmgr install ieeetrans ctex
+cd ..
+sudo /usr/local/texlive/2022/bin/x86_64-linux/tlmgr install ieeetrans ctex latexindent
+wget http://cpanmin.us/ -O cpanm
+chmod +x cpanm
+sudo ./cpanm File::HomeDir YAML::Tiny
 echo "export PATH=$PATH:/usr/local/texlive/2022/bin/x86_64-linux" >>~/.bashrc
